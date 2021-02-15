c24384d182945b5e86bccb03adfa935d6ec5a9fd
trum <bardabarda228@gmail.com>
2021-02-15 09:53:59 +0500

    First code implementing

---
+++ main.py
@@ -0,0 +1,17 @@
+import socket
+
+sock = socket.socket()
+sock.bind(('', 9090))
+sock.listen(1)
+conn, addr = sock.accept()
+
+print 'connected:', addr
+
+while True:
+    data = conn.recv(1024).decode()
+    if data.lower() == 'close':
+        break
+    if data:
+        conn.send(data.upper().encode())
+
+conn.close()
